db21c94d79f0eaf4d488010c8a5a1f24dbce7d7f
ryuka <nohararyuka7@gmail.com>
2022-08-11 23:08:12 +0900

    無駄なファイルを削除

--- front/app/src/pages/_app.tsx
+++ front/app/src/pages/_app.tsx
@@ -1,8 +1,8 @@
-import React from "react";
+import { FC } from "react";
 import { AppProps } from "next/app";
 import "../styles/globals.css";
 
-const MyApp = ({ Component, pageProps }: AppProps) => {
+const MyApp: FC = ({ Component, pageProps }: AppProps) => {
   return <Component {...pageProps} />;
 };
 
